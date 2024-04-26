366db123210e2dc36792742ac72bcd8f4e5dc39e
Marcos <marcosgf.dev@gmail.com>
2024-04-26 14:48:21 +0200

    added new map

--- game.sh
+++ game.sh
@@ -3,12 +3,11 @@ as len_str.s -o len_str.o --32 -g
 as print_str.s -o print_str.o --32 -g
 as set_cursor_pos.s -o set_cursor_pos.o --32 -g
 as laberintoBASE.s -o laberintoBASE.o --32 -g
-as load_map.s -o load_map.o --32 -g
-ld str_to_ul.o laberintoBASE.o print_str.o len_str.o load_map.o set_cursor_pos.o -o game -melf_i386
+ld str_to_ul.o laberintoBASE.o print_str.o len_str.o set_cursor_pos.o -o game -melf_i386
 rm laberintoBASE.o
 rm load_map.o
 rm str_to_ul.o
 rm set_cursor_pos.o
 rm print_str.o
 rm len_str.o
-./game ./maps/map1.bin 2 2 5 2
+./game ./maps/map2.bin 2 2 5 2
